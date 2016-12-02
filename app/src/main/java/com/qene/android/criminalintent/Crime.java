ced947154afcbcb9a5bc30f7adeb6bce85fa2fd6
daveScof <davescofab@gmail.com>
2016-12-02 15:57:39 +0300

    Create Crime Class

---
+++ app/src/main/java/com/qene/android/criminalintent/Crime.java
@@ -0,0 +1,29 @@
+package com.qene.android.criminalintent;
+
+import java.util.UUID;
+
+/**
+ * Created by dave-5cof on 12/2/16.
+ */
+
+public class Crime {
+    private UUID mID;
+    private String mTitle;
+
+    public Crime(){
+        mID = UUID.randomUUID();
+    }
+
+    public UUID getmID() {
+        return mID;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+}
