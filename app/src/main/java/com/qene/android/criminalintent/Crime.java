85bf0d5c98bc31d0c6c4fa854157e7fad3f6a93f
daveScof <davescofab@gmail.com>
2016-12-03 23:12:54 +0300

    Add Date and Solved fields to Crime Class

--- app/src/main/java/com/qene/android/criminalintent/Crime.java
+++ app/src/main/java/com/qene/android/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.qene.android.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -9,21 +10,39 @@ import java.util.UUID;
 public class Crime {
     private UUID mID;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
 
     public Crime(){
         mID = UUID.randomUUID();
+        mDate = new Date();
     }
 
-    public UUID getmID() {
+    public UUID getID() {
         return mID;
     }
 
-    public String getmTitle() {
+    public String getTitle() {
         return mTitle;
     }
 
-    public void setmTitle(String mTitle) {
-        this.mTitle = mTitle;
+    public void setTitle(String title) {
+        mTitle = title;
     }
-
 }
