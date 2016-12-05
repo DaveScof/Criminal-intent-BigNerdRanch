8da83422d177bfda6a61a43723b843b7e645a90b
daveScof <davescofab@gmail.com>
2016-12-05 14:34:38 +0300

    Add CrimeLab class to manage more than one Crime Objects

--- app/src/main/java/com/qene/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/qene/android/criminalintent/CrimeLab.java
@@ -1,8 +1,25 @@
 package com.qene.android.criminalintent;
 
+import android.content.Context;
+
 /**
  * Created by dave-5cof on 12/5/16.
  */
 
 public class CrimeLab {
+
+    public static CrimeLab sCrimeLab;
+
+    public static CrimeLab get(Context context) {
+
+        if (sCrimeLab == null){
+            sCrimeLab = new CrimeLab();
+        }
+        return sCrimeLab;
+    }
+
+    private CrimeLab (){
+    }
+
+
 }
