ad325f4e16fd33c13968eded9408c79f898d75bc
daveScof <davescofab@gmail.com>
2016-12-05 16:33:10 +0300

    Create Abstract Activity SingleFragmentActivity to handle generic Fragment Hosting Activity

--- app/src/main/java/com/qene/android/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/qene/android/criminalintent/SingleFragmentActivity.java
@@ -1,10 +1,30 @@
 package com.qene.android.criminalintent;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 
 /**
  * Created by dave-5cof on 12/5/16.
  */
 
-public abstract class SingleFragmentActivity extends Fragment {
+public abstract class SingleFragmentActivity extends FragmentActivity {
+
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null){
+            fragment = createFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+    }
 }
