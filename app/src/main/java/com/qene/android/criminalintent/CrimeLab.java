3a80e16f682ad1f07c87373fa26416c76c0c221d
daveScof <davescofab@gmail.com>
2016-12-05 16:32:05 +0300

    Add a List in CrimeLab that will hold Crime Objects and populate it with 100 fake objects

--- app/src/main/java/com/qene/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/qene/android/criminalintent/CrimeLab.java
@@ -2,6 +2,11 @@ package com.qene.android.criminalintent;
 
 import android.content.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+
 /**
  * Created by dave-5cof on 12/5/16.
  */
@@ -9,6 +14,7 @@ import android.content.Context;
 public class CrimeLab {
 
     public static CrimeLab sCrimeLab;
+    private List<Crime> mCrimes;
 
     public static CrimeLab get(Context context) {
 
@@ -19,7 +25,25 @@ public class CrimeLab {
     }
 
     private CrimeLab (){
+        mCrimes = new ArrayList<>();
+        for (int i = 1; i <= 100; i++) {
+            Random rand = new Random();
+            Crime crime = new Crime();
+            crime.setSolved(rand.nextInt(1) == 0);
+            crime.setTitle("Crime #" + i);
+            mCrimes.add(crime);
+        }
     }
 
+    public List<Crime> getCrimes() {
+        return mCrimes;
+    }
 
+    public Crime getCrime(UUID uuid) {
+        for (Crime crime: mCrimes) {
+            if (crime.getID() == uuid)
+                return crime;
+        }
+        return null;
+    }
 }
