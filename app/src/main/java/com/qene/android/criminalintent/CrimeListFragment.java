b548faccf63cea2f23292c58bff5057bfdbe80cc
daveScof <davescofab@gmail.com>
2016-12-10 20:04:24 +0300

    make CrimeListFragment start CrimeActivity when a list item is pressed

--- app/src/main/java/com/qene/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/qene/android/criminalintent/CrimeListFragment.java
@@ -1,5 +1,6 @@
 package com.qene.android.criminalintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -70,7 +71,8 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View view) {
-            Toast.makeText(getActivity(), mCrime.getTitle(), Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(getActivity(), CrimeActivity.class);
+            startActivity(intent);
         }
     }
 
