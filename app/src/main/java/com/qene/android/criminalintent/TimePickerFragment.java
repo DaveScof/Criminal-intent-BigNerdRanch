32a8f81945ed14c2d821900436d56e0fae9ccb12
daveScof <davescofab@gmail.com>
2016-12-12 23:58:33 +0300

    Add newInstance Method to TimePickerFragment to pass Arguments to itself

--- app/src/main/java/com/qene/android/criminalintent/TimePickerFragment.java
+++ app/src/main/java/com/qene/android/criminalintent/TimePickerFragment.java
@@ -2,6 +2,7 @@ package com.qene.android.criminalintent;
 
 
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
@@ -10,12 +11,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TimePicker;
 
+import java.sql.Time;
+
 /**
  * Created by dave-5cof on 12/12/16.
  */
 
 public class TimePickerFragment extends DialogFragment {
     private TimePicker mTimePicker;
+    private static final String ARG_TIME = "time";
 
 
     @NonNull
@@ -28,8 +32,23 @@ public class TimePickerFragment extends DialogFragment {
         return new AlertDialog.Builder(getActivity())
                 .setView(view)
                 .setTitle(R.string.time_picker_title)
-                .setPositiveButton(android.R.string.ok, null)
-                .create();
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
 
+                    }
+                })
+                .create();
     }
+
+    public static TimePickerFragment newInstance (Time time){
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_TIME, time);
+
+        TimePickerFragment fragment = new TimePickerFragment();
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+
 }
