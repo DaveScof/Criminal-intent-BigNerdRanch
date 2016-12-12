ec343b63f744d7cf4c34cc2d0249c32bf3ab6ec6
daveScof <davescofab@gmail.com>
2016-12-12 20:04:29 +0300

    Add DialogFragment Class and dialog_date.xml layout

---
+++ app/src/main/java/com/qene/android/criminalintent/DatePickerFragment.java
@@ -0,0 +1,22 @@
+package com.qene.android.criminalintent;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+
+/**
+ * Created by dave-5cof on 12/12/16.
+ */
+
+public class DatePickerFragment extends DialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.date_picker_title)
+                .setPositiveButton(android.R.string.ok, null)
+                .create();
+    }
+}
