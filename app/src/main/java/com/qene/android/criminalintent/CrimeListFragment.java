5f3cee81f7e0920b5ad54fc366def2659236c613
daveScof <davescofab@gmail.com>
2016-12-16 20:06:32 +0300

    Make Show/Hide Subtitle menu item correspond with actual state

--- app/src/main/java/com/qene/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/qene/android/criminalintent/CrimeListFragment.java
@@ -17,7 +17,6 @@ import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.List;
 
@@ -31,6 +30,7 @@ public class CrimeListFragment extends Fragment {
 
     private RecyclerView mCrimeRecyclerView;
     private CrimeAdapter mAdapter;
+    private boolean mSubtitleVisible;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -143,6 +143,14 @@ public class CrimeListFragment extends Fragment {
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.fragment_crime_list, menu);
+
+        MenuItem subtitleItem = menu.findItem(R.id.menu_item_show_subtitle);
+        if(mSubtitleVisible){
+            subtitleItem.setTitle(R.string.hide_subtitle);
+        }
+        else {
+            subtitleItem.setTitle(R.string.show_subtitle);
+        }
     }
 
     @Override
@@ -155,6 +163,8 @@ public class CrimeListFragment extends Fragment {
                 startActivityForResult(intent, REQUEST_CODE_DATA_CHANGE);
                 return true;
             case (R.id.menu_item_show_subtitle):
+                mSubtitleVisible = !mSubtitleVisible;
+                getActivity().supportInvalidateOptionsMenu();
                 updateSubtitle();
                 return true;
             default:
@@ -167,6 +177,9 @@ public class CrimeListFragment extends Fragment {
         int crimeCount = crimeLab.getCrimes().size();
 
         String subtitle = getString(R.string.subtitle_format, crimeCount);
+        if (!mSubtitleVisible)
+            subtitle = null;
+
         AppCompatActivity activity = (AppCompatActivity) getActivity();
 
         activity.getSupportActionBar().setSubtitle(subtitle);
