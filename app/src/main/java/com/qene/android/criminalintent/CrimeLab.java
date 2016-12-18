852e7e8bbc04b725b64ef2447b98fd5ae1cc4cae
daveScof <davescofab@gmail.com>
2016-12-19 01:03:14 +0300

    Add static getContentValues method to CrimeLab

--- app/src/main/java/com/qene/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/qene/android/criminalintent/CrimeLab.java
@@ -1,13 +1,14 @@
 package com.qene.android.criminalintent;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.qene.android.criminalintent.database.CrimeBaseHelper;
+import com.qene.android.criminalintent.database.CrimeDbSchema.CrimeTable;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.UUID;
 
 /**
@@ -51,6 +52,16 @@ public class CrimeLab {
 
     public int getPosition (Crime crime){
         return 0;
+    }
+
+    private static ContentValues getContentValues (Crime crime){
+        ContentValues values = new ContentValues();
+        values.put(CrimeTable.Cols.UUID, crime.getID().toString());
+        values.put(CrimeTable.Cols.TITLE, crime.getTitle());
+        values.put(CrimeTable.Cols.DATE, crime.getDate().getTime());
+        values.put(CrimeTable.Cols.SOLVED, crime.isSolved() ? 1 : 0);
+
+        return values;
     }
 
 }
