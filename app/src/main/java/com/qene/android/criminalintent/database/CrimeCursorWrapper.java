7bb7c044caca57100b3134ec4488429ec9ba0957
daveScof <davescofab@gmail.com>
2016-12-19 02:04:24 +0300

    Get crime from the cursor that was retrieved from the Database and create a Crime Object from it

--- app/src/main/java/com/qene/android/criminalintent/database/CrimeCursorWrapper.java
+++ app/src/main/java/com/qene/android/criminalintent/database/CrimeCursorWrapper.java
@@ -1,10 +1,27 @@
 package com.qene.android.criminalintent.database;
 
+import android.database.Cursor;
 import android.database.CursorWrapper;
+
+import com.qene.android.criminalintent.Crime;
+import com.qene.android.criminalintent.database.CrimeDbSchema.CrimeTable;
 
 /**
  * Created by dave-5cof on 12/19/16.
  */
 
 public class CrimeCursorWrapper extends CursorWrapper {
+    public CrimeCursorWrapper(Cursor cursor) {
+        super(cursor);
+    }
+
+    public Crime getCrime (){
+        String uuidString = getString(getColumnIndex(CrimeTable.Cols.UUID));
+        String title = getString(getColumnIndex(CrimeTable.Cols.TITLE));
+        long date = getLong(getColumnIndex(CrimeTable.Cols.DATE));
+        int isSolved = getInt(getColumnIndex(CrimeTable.Cols.SOLVED));
+
+        return null;
+    }
+
 }
