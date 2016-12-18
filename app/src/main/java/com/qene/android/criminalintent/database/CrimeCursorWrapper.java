97aaae078402d2f34e8bf0fa33e1a9e42192e636
daveScof <davescofab@gmail.com>
2016-12-19 02:02:21 +0300

    Add CrimeCursorWrapper

---
+++ app/src/main/java/com/qene/android/criminalintent/database/CrimeCursorWrapper.java
@@ -0,0 +1,10 @@
+package com.qene.android.criminalintent.database;
+
+import android.database.CursorWrapper;
+
+/**
+ * Created by dave-5cof on 12/19/16.
+ */
+
+public class CrimeCursorWrapper extends CursorWrapper {
+}
