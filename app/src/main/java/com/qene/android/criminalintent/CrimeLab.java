58f13ad90b1c81a3dccfbb8a6966a43447b47a1d
daveScof <davescofab@gmail.com>
2017-01-10 14:39:20 +0300

    get crime photo file
    Add method that the external storage location and the crime's getPhotoFileName to locate the photo file of the crime

--- app/src/main/java/com/qene/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/qene/android/criminalintent/CrimeLab.java
@@ -4,11 +4,13 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Environment;
 
 import com.qene.android.criminalintent.database.CrimeBaseHelper;
 import com.qene.android.criminalintent.database.CrimeCursorWrapper;
 import com.qene.android.criminalintent.database.CrimeDbSchema.CrimeTable;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -122,5 +124,14 @@ public class CrimeLab {
                 null
         );
         return new CrimeCursorWrapper(cursor);
+    }
+
+    public File getPhotoFile (Crime crime){
+        File externalFilesDir = mContext.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        if (externalFilesDir == null)
+            return null;
+
+        return new File(externalFilesDir,crime.getPhotoFileName());
     }
 }
